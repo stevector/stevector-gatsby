b32fde111370bf701300b30a9132680f8603a849
Steve Persch <stevector@users.noreply.github.com>
2019-12-27 14:23:37 -0600

    F07bfe color

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#007bfe`,
+      background: `#F07bfe`,
       marginBottom: `1.45rem`,
     }}
   >
