4075133e86e77600999dac770d8cc8a4e113da31
Steve Persch <steve.persch@pantheon.io>
2019-12-27 14:35:12 -0600

    Revert "F07bfe color"
    This reverts commit b32fde111370bf701300b30a9132680f8603a849.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#F07bfe`,
+      background: `#007bfe`,
       marginBottom: `1.45rem`,
     }}
   >
