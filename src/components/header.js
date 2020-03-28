de49e3cbb3f2cd681fd4bc71e98586cb7cbdbd8c
Steve Persch <stevector@users.noreply.github.com>
2020-03-28 15:56:16 -0500

    [ci skip]

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#007bff`,
+      background: `#FF0000`,
       marginBottom: `1.45rem`,
     }}
   >
