4b8bd059251f50424e3194abbb0089d3d79a0d4a
Steve Persch <steve.persch@pantheon.io>
2020-03-28 18:38:05 -0500

    slateBlue

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#FF0000`,
+      background: `#6A5ACD`,
       marginBottom: `1.45rem`,
     }}
   >
