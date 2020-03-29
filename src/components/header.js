b183a3fd8efb527e391262ac18f4e34606599b3f
Steve Persch <stevector@users.noreply.github.com>
2020-03-29 11:13:11 -0500

    Use orb (#15)
    * Update config.yml
    
    * adding comments and updating branch checking logic
    
    * Revert "adding comments and updating branch checking logic"
    
    This reverts commit 367ca8f0679121f38068be3d4cd3f79ed66b2c52.
    
    * fix nesting
    
    * bringing back comments
    
    * comment cleanup
    
    * env setting cleanup
    
    * comment update
    
    * use orb to set terminus_env
    
    * use orb job
    
    * gatsby build and deploy from orb
    
    * red header
    
    * whitespace
    
    * fixing reference
    
    * shorter orb name
    
    * back to blue

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#111111`,
+      background: `#007bff`,
 
       marginBottom: `1.45rem`,
     }}
