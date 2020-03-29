d9fabe1abc11903239ca81f969b51789c3611eb8
Steve Persch <steve.persch@pantheon.io>
2020-03-28 21:16:33 -0500

    circle branch filter

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#6A5ACD`,
+      background: `#333333`,
       marginBottom: `1.45rem`,
     }}
   >
