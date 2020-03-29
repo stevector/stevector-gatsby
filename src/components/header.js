57c39aaeecffa0e9a47d24cd2bed15abaf90f6cd
Steve Persch <steve.persch@pantheon.io>
2020-03-29 11:06:11 -0500

    shorter orb name

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#FF0000`,
+      background: `#333333`,
 
       marginBottom: `1.45rem`,
     }}
