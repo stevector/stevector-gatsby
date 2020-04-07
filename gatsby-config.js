14c3eeb7c76f4c0b26f4d8d520d43188f781bf06
Steve Persch <stevector@users.noreply.github.com>
2020-04-06 21:05:12 -0500

    Twitter (#29)
    * Delete package-lock.json
    
    * yarn lock
    
    * google analytics
    
    * yarn update
    
    * remove unused text
    
    * twitter link
    
    * twitter
    
    * prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: " UA-42048647-1"
+        trackingId: " UA-42048647-1",
       },
     },
     {
@@ -20,6 +20,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
       },
     },
     {
