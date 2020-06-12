59b145b3296c7e9de7989bde27c69dc9626d23bb
Steve Persch <stevector@users.noreply.github.com>
2020-06-12 11:01:01 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        start_url: `/`,
+        start_url: `/nested-example/site`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
